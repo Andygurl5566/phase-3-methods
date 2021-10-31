9a7274c4e06e1d7f254cdc86c1d22b0cefd920eb
Andrea Freeman <afreeman5566@gmail.com>
2021-10-30 19:40:16 -0500

    commit tree a4e4406cea5a15b379ff2acc9ef238f36800b52e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,47 @@
+
+require "pry"
+
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+#2
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+
+#3
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+#4
+
+def add(num1, num2)
+    num1 + num2
+end
+
+add(3,4)
+
+#5
+
+def halve(number)
+    if (number.class != Integer) 
+    return nil
+    end
+    
+    return number/2
+    end
+
+    halve(6)
